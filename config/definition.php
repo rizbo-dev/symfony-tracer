317d74c09fc8c93c54d287dbfaf5c6c10759ce72
Boris Evetovic <boris.evetovic777@gmail.com>
2025-05-15 23:36:31 +0200

    Setting up folder structure

---
+++ config/definition.php
@@ -0,0 +1,9 @@
+<?php
+
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+
+return static function (DefinitionConfigurator $definition) {
+    $definition->rootNode()
+        ->children()
+        ->end();
+};
